4427b164d9ed874ab81e0e503975f3a6f9a0effc
Louise <louisepeiffer@hotmail.com>
2021-01-06 12:20:47 +0100

    Fini mas o menos

--- public/js/main.js
+++ public/js/main.js
@@ -42,16 +42,67 @@
 // )
 
 // 6
-let abon = confirm('Voulez-vous vous abonner?')
-console.log(abon);
-if (abon == true) {
-    let which = confirm('Desirez-vous la formule Luxe ou Normale?')
-    if (which = true) {
-        alert('Bravo!')
-    }
-    else {
-        alert('Cool!')
-    }
-}
+// let abon = confirm('Voulez-vous vous abonner?')
+// console.log(abon);
+// if (abon == true) {
+//     let which = confirm('Desirez-vous la formule Luxe ou Normale?')
+//     if (which == true) {
+//         alert('Bravo!')
+//     }
+//     else if (which == false) {
+//         alert('Merci pour votre abonnement')
+//     }
+// }
 
+
+// // 7
+// let quest1 = prompt("J'ai quel âge?")
+// let rep1 = '22'
+// let quest2 = prompt("Ma filleule s'appelle?")
+// let rep2 = 'Tita'
+// if (quest1 == rep1 && quest2==rep2) {
+//     alert('Bv')
+// }
+// else if (quest1 == rep1 || quest2 == rep2) {
+//     alert('Presque')
+// }
+// else if (quest1!=rep1 && quest2!=rep2) {
+//     alert('t nul')
+// }
+
+// 8
+// let cine = parseInt(prompt('Bienvenue au Kinepolis! Quel âge avez-vous?'))
+// if (cine > 18) {
+//     alert('Vous êtes majeur, vous pouvez réserver')
+// }
+// else if (cine<18) {
+//     alert('T trop petit')
+// }
+// else if (cine==18) {
+//     alert('vasy')
+// }
+// else if (cine < 0) {
+//     alert('Reste dans le ventre de ta mams')
+// }
+// else if (cine > 100) {
+//     alert('Degach papy')
+// }
+
+// 9 
+// let riche = confirm('Êtes-vous riche?')
+// let vac = confirm('Voulez-vous partir en vacances?')
+// let chat = confirm('Avez-vous un chat?')
+
+// if (vac == false) {
+//     alert("shii if you don't go I will")
+// }
+// else if (riche==false || chat==true) {
+//     alert("Tu peux pas d'façon t'as un chat")
+// }
+// else if (riche==true && chat==false) {
+//     alert("Leggoo")
+// }
+// else if (riche==true && chat==false && vac==true) {
+//     alert("Go ahead gurl")
+// }
 
